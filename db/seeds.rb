593fc531171415f2bfc5ac815ee3ff149a3b491f
wanxin pua <wanxinpua@yahoo.com.sg>
2022-05-26 14:14:27 +0800

    rails with restaurants and reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "cleaning database"
+Restaurant.destroy_all
+
+categories = ["chinese", "italian", "japanese", "french", "belgian"]
+10.times do
+  @restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164 || Faker::PhoneNumber.cell_phone || Faker::PhoneNumber.phone_number_with_country_code,
+    address: Faker::Address.street_address,
+    category: categories.sample
+  )
+  @restaurant.save!
+  puts "#{@restaurant.name} created"
+end
